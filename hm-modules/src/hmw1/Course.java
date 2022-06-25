2531ef22119c6736a2f4e23a8565bff3b1dc4d6d
Yaren <yareenm@outlook.com>
2022-06-25 03:35:08 -0500

    updated about cardinality and comments

--- hm-modules/src/hmw1/Course.java
+++ hm-modules/src/hmw1/Course.java
@@ -1,15 +1,19 @@
 package hmw1;
+import java.util.Objects;
 
+// Class name is Course. It has 3 attributes; related course name, course code and credit score.
+// e-mail: yareenm@outlook.com
 public class Course {
     private String courseName;
     private String courseCode;
     private int creditScore;
 
+    private Instructor instructor = new Instructor();
     public Course(String courseName, String courseCode, int creditScore){
         this.courseName = courseName;
         this.courseCode = courseCode;
         this.creditScore = creditScore;
-    }
+    }//constructor
 
     public String getCourseName() {
         return courseName;
@@ -33,5 +37,27 @@ public class Course {
 
     public void setCreditScore(int creditScore) {
         this.creditScore = creditScore;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Course course = (Course) o;
+        return creditScore == course.creditScore && Objects.equals(courseName, course.courseName) && Objects.equals(courseCode, course.courseCode);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(courseName, courseCode, creditScore);
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "courseName='" + courseName + '\'' +
+                ", courseCode='" + courseCode + '\'' +
+                ", creditScore=" + creditScore +
+                '}';
     }
 }
